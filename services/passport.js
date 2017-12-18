19ce90ed3bd66a1c7a7961538333214c3a8c24bc
Susan Flanagan <susanflanagan@Susans-MBP.home>
2017-12-18 00:42:43 -0500

    refactored promise

--- services/passport.js
+++ services/passport.js
@@ -20,19 +20,14 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                    console.log('User already exists');
-                    done(null, existingUser);
-                } else {
-                    new User({ googleId: profile.id }).save()
-                        .then(user => {
-                            done(null, user);
-                        });
-                }
-            })
-        
-    })
-);
+}, async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if (existingUser) {
+            console.log('User already exists');
+            done(null, existingUser);
+        } else {
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);
+        }  
+    }
+));
