3d82c14adfbf12367ddcc806735fdc019fc4da4e
Susan Flanagan <susanflanagan@Susans-MBP.home>
2017-12-17 23:01:09 -0500

    added database and finished oauth

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const express = require('express');
+const   express = require('express'),
+        mongoose = require('mongoose'),
+        cookieSession = require('cookie-session'),
+        passport = require('passport'),
+        keys = require('./config/keys');
+
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello!");
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 6 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Routes
+require('./routes/authRoutes')(app);
+
 
 app.listen(process.env.PORT || 5000);
